1128fce10320c76c0ad4b3036a44cf4d27a93ad9
rika-t <rika-t@evaluni.com>
2018-08-22 12:24:23 +0900

    add firebase and firebase-ui

---
+++ client/src/client_config.ts
@@ -0,0 +1,22 @@
+export class ClientConfig {
+  static LOCAL = {
+    firebase: {
+      apiKey: null,
+      authDomain: null,
+      databaseURL: null,
+      projectId: null,
+      storageBucket: null,
+      messagingSenderId: null
+    }
+  };
+  static DEV = {
+    firebase: {
+      apiKey: null,
+      authDomain: null,
+      databaseURL: null,
+      projectId: null,
+      storageBucket: null,
+      messagingSenderId: null
+    }
+  }
+}
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,27 @@
 import * as Rx from "rxjs";
 import { ApiCaller } from "./lib/api-caller";
 import { map } from "rxjs/operators";
+import firebase from "firebase/app";
+import 'firebase/auth'
+import * as firebaseui from "firebaseui";
+import {ClientConfig} from "./client_config";
 
 (function () {
   let queries: any = window.location.search.substring(1).split("&").map(query => query.split('='))
     .reduce((result, [k, v]) => Object.assign(result, { [k]: decodeURI(v) }), {});
+
+  firebase.initializeApp(ClientConfig.LOCAL.firebase);
+  let ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+  ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+    ],
+  });
 
   let subscriptions = new Rx.Subscription();
   subscriptions.add(
